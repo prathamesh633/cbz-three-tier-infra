8865c23ce39701770c5c6268382fc7255ac54404
prathamesh633 <prathmeshbhujade1234@gmail.com>
2025-04-26 10:20:41 +0530

    added new repo and changed region and bucket name

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,6 @@
 # Create an S3 bucket
 resource "aws_s3_bucket" "cbz_bucket" {
-  bucket = "cbz-frontend-project-bux" # Replace with a globally unique bucket name
+  bucket = "prathameshsye3bucket633" # Replace with a globally unique bucket name
 
   # Enable static website hosting
   website {
